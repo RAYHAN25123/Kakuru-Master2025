269670a7eb86b84300ae5c4c44386828175b9d86
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-09-27 11:38:15 +0700

    Update TEORIMA_PYTHAGORAS.py

--- pages/TEORIMA_PYTHAGORAS.py
+++ pages/TEORIMA_PYTHAGORAS.py
@@ -1,1 +1,13 @@
 import streamlit as st
+
+st.title("Teorima Pythagoras")
+st.markdown("""
+###Penjabaran :
+- a = Sisi siku-siku (Alas)
+- b = Sisi siku-siku(Tinggi)
+- c = Sisi miring
+###RUMUS :
+- c2 = a2 + b2
+- a2 = c2 - b2
+- b2 = c2 - a2
+""")
