3b0bbc11076034ba052f4050cf16fb87bd2a3024
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-10-02 20:32:37 +0700

    Update TEORIMA_PYTHAGORAS.py

--- pages/TEORIMA_PYTHAGORAS.py
+++ pages/TEORIMA_PYTHAGORAS.py
@@ -14,9 +14,17 @@ st.markdown("""
 """)
 
 st.subheader("Mencari C")
-a = st.number_input("Masukan a")
-b = st.number_input("Masukan b")
+a = st.number_input("Masukan a, key="a2")
+b = st.number_input("Masukan b, key="b2")
 c = a **2 + b **2
 c1 = c **0.5
 st.header("Hasil")
 st.subheader(c1)
+
+st.subheader("Mencari B")
+c2 = st.number_input("Masukan c, key="c2")
+a1 = st.number_input("Masukan a, key="a1")
+b1 = c2 **2 - a2 **2
+b2 = b1 **0.5
+st.header("Hasil")
+st.subheader(b2)
