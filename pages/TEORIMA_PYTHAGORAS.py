32564fffa59e1b1a0de078ca97852f5e8a7f5c24
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-10-02 21:10:03 +0700

    Update TEORIMA_PYTHAGORAS.py

--- pages/TEORIMA_PYTHAGORAS.py
+++ pages/TEORIMA_PYTHAGORAS.py
@@ -13,4 +13,10 @@ st.markdown("""
 - b2 = c2 - a2
 """)
 
-st.subheader("")
+st.subheader("Mencari C (Sisi Mriring)")
+a = st.number_input("Masukkan panjang sisi a:", min_value=0.0)
+b = st.number_input("Masukkan panjang sisi b:", min_value=0.0)
+c = a **2 + b **2
+c1 = c **0.5
+st.header("Hasil C (Sisi Miring)")
+st.subheader(c1)
