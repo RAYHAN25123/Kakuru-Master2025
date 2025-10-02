d5210575039450208b1b6af4523aef17f3e959b3
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-10-02 20:51:15 +0700

    Update TEORIMA_PYTHAGORAS.py

--- pages/TEORIMA_PYTHAGORAS.py
+++ pages/TEORIMA_PYTHAGORAS.py
@@ -2,18 +2,15 @@ import streamlit as st
 import math
 
 st.title("Teorema Pythagoras")
+st.markdown("""
+### Penjabaran :
+- c = Sisi Miring
+- a = Alas
+- b = Tinggi
+### RUMUS :
+- c2 = a2 + b2
+- a2 = c2 - b2
+- b2 = c2 - a2
+""")
 
-if pilihan == "Sisi Miring (c)":
-    a = st.number_input("Masukkan panjang sisi a:", min_value=0.0)
-    b = st.number_input("Masukkan panjang sisi b:", min_value=0.0)
-    if a > 0 and b > 0:
-        c = math.sqrt(a**2 + b**2)
-        st.success(f"✅ Panjang sisi miring (c) = {c:.2f}")
-
-elif pilihan == "Sisi Siku (a atau b)":
-    c = st.number_input("Masukkan panjang sisi miring (c):", min_value=0.0)
-    x = st.number_input("Masukkan panjang sisi siku yang diketahui:", min_value=0.0)
-    if c > 0 and x > 0 and c > x:
-        sisi_lain = math.sqrt(c**2 - x**2)
-        st.success(f"✅ Panjang sisi siku yang belum diketahui = {sisi_lain:.2f}")
-    elif c > 0 and x > 0 and c <= x:
+st.subheader("")
