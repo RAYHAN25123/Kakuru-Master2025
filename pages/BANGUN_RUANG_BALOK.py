e183e192e5d2d0d7e5f8c44b38a4b4ca8b1895ed
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-10-02 20:56:09 +0700

    Update BANGUN_RUANG_BALOK.py

--- pages/BANGUN_RUANG_BALOK.py
+++ pages/BANGUN_RUANG_BALOK.py
@@ -1,6 +1,23 @@
 import streamlit as st
 
 st.title("BANGUN RUANG BALOK")
+
+st.markdown("""
+### Penjabaran :
+- p = Panjang
+- l = Lebar
+- t = Tinggi
+- K = Keliling
+- L = Luas
+- V = Volume
+### Rumus Mencari Keliling :
+- K = 4 x (p + l + t)
+### Rumus Mencari Luas :
+- L = 2 x (p x l + p x t + l x t)
+### Rumus Mencari Volume :
+- V = p x l x t
+
+st.subheader("Balok")
 p = st.number_input("Masukan Nilai Panjang:")
 l = st.number_input("Masukan Nilai Lebar:")
 t = st.number_input("Masukan Nilai Tinggi:")
