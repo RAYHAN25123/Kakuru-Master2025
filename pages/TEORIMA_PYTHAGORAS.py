9d5548011ac7b5de2fa2ab0331e18c1b0c2baafc
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-10-02 21:17:22 +0700

    Update TEORIMA_PYTHAGORAS.py

--- pages/TEORIMA_PYTHAGORAS.py
+++ pages/TEORIMA_PYTHAGORAS.py
@@ -24,7 +24,7 @@ st.subheader(c1)
 st.subheader("Mencari a (Alas)")
 c2 = st.number_input("Masukkan panjang sisi miring c:", min_value=0.0)
 a1 = st.number_input("Masukkan panjang sisi b:", min_value=0.0, key= "key b")
-b1 = c2 **2 + a1 **2
+b1 = c2 **2 - a1 **2
 b2 = b1 **0.5
 st.header("Hasil a (Alas)")
 st.subheader(b2)
